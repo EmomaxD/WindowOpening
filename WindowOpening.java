827b8293862ab048bcb0ce6e4591b0eb2f00be94
Emre <emreleno@gmail.com>
2022-01-02 01:25:31 +0300

    not working yet

--- WindowOpening.java
+++ WindowOpening.java
@@ -1,13 +1,17 @@
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
+import org.json.*;
+import java.io.PrintWriter;
+import java.io.File;
+
 
   
 
 
 class WindowOpening{
      static String api_URL = "http://api.weatherapi.com/v1/current.json?key=c5029df5d2c34a07b53154946220101&q=Sakarya&aqi=no";
-     static String weatherInfo = "";
+     static String weatherData;
      static URL url;
      static Scanner scanner;
      static String humidity;
@@ -16,30 +20,70 @@ class WindowOpening{
      static String airCondition;
      static String feltTemperature;
      
+     static String data;
+     static JSONObject wJ = new JSONObject();
+
+
       
 
     public static void main(String[] args) throws IOException {
-
+        
+        //System.out.println(getFileData());
+        //WeatherInfo("location");
+        
+        System.out.println(getFileData());
+        //System.out.println(WeatherInfo(""));
+        
         System.out.println(feltTemperature(90,40));
-        System.out.println(WeatherInfo());
-        weatherInfo=WeatherInfo().toString();       
+        
+              
     }
 
     //Functions
-    static List<String> WeatherInfo()throws IOException{
-        List<String> info =new ArrayList<>();
+    static String getFileData()throws IOException{
+        File myObj = new File("weatherData.json");
+        myObj.delete();
+        PutDataToJSON(wJ);
+        CreateJsonFile();
+        Scanner myReader = new Scanner(myObj);
+
+        while (myReader.hasNextLine()) {
+          data = myReader.nextLine();
+        }
+        myReader.close();
+        return data;
+    }
+    static String WeatherInfo(String e)throws IOException{
+        JSONObject obj = new JSONObject(data);
+         e =obj.getString("general");
+         
+         
+
+
+        return e;
+    }
+    static void PutDataToJSON(JSONObject js)throws IOException{
+        
         url=new URL(api_URL);
         scanner = new Scanner(url.openStream());
         while(scanner.hasNext()){
             //System.out.println(scanner.nextLine());
             
-
-            info.add(scanner.nextLine());
-            
+           js.put("general", scanner.nextLine());
+        
         }
+          
 
+       
+    }
+    static void CreateJsonFile()throws IOException{
+        // writing JSON to file:"JSONExample.json" in cwd
+        PrintWriter pw = new PrintWriter("weatherData.json");
+        pw.write(wJ.toString());
 
-        return info;
+        pw.flush();
+        pw.close();
+
     }
     static double feltTemperature(double T,double H){
         double c1 = -42;
